0a773ddc30796e3c3dcd87c9cfe2f7c5c8cbe8ed
kianaakbari <kianaakbari@yahoo.com>
2016-04-14 09:19:08 +0430

    Added files via upload

---
+++ create slide.js
@@ -0,0 +1,14 @@
+/**
+ * Created by samane on 11/04/2016.
+ */
+
+//$(document).ready(function() {
+//
+//    $('#header-input').css('display': 'inline-block');
+//});
+
+
+$("#title-btn").click(function(){
+    alert("hii");
+    $('#header-input').css('visibility', 'visible');
+});
