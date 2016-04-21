a986eab4d2b0b46f74717ecf1bb5406541e7fe23
kianaakbari <kianaakbari@yahoo.com>
2016-04-21 20:11:25 +0430

    add login and register js

---
+++ js/controllers/register.js
@@ -0,0 +1,31 @@
+
+    var app = angular.module("registerApp", []);
+    app.controller("registerAppController", function ($scope, $http) {
+
+        $scope.SendData = function () {
+            // use $.param jQuery function to serialize data from JSON
+
+            var data = ({
+                email: $scope.email,
+                firstname: $scope.firstname,
+                lastname: $scope.lastname,
+                password: $scope.password,
+                is_audience: $scope.is_audience
+            });
+
+            var config = {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+
+            $http.post('http://127.0.0.1:8000/api/v1/register', (data), config)
+                    .success(function (data, status, headers, config) {
+                        $scope.PostDataResponse = data;
+                    })
+                    .error(function (data, status, header, config) {
+                        $scope.PostDataResponse = data;
+                    });
+        };
+
+    });
