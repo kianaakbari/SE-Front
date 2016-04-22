522f0237023d16e3df8a6f288178c9a113a346bc
kianaakbari <kianaakbari@yahoo.com>
2016-04-22 21:38:13 +0430

    Update register.js

--- js/controllers/register.js
+++ js/controllers/register.js
@@ -21,13 +21,9 @@
 
             $http.post('http://127.0.0.1:8000/api/v1/register', (data), config)
                     .success(function (data, status, headers, config) {
-                        $scope.PostDataResponse = data;
-                        $scope.PostStatusResponse = status;
                         // window.location="Presenter Page.html";
                     })
                     .error(function (data, status, header, config) {
-                        $scope.PostDataResponse = data;
-                        $scope.PostStatusResponse = status;
                         // if(status==406) {document.getElementById("wrong_password").style.visibility="visible";}
                         // if(status==401) {document.getElementById("wrong_password").style.visibility="visible";}
                     });
